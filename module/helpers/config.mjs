6200b9c1c157c61442e73334dd479b204495f80a
matt-pk <39395476+matt-pk@users.noreply.github.com>
2024-05-18 17:29:02 +1000

    Boilerplate
    As per https://gitlab.com/asacolips-projects/foundry-mods/boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const THRESHOLD = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+THRESHOLD.abilities = {
+  str: 'THRESHOLD.Ability.Str.long',
+  dex: 'THRESHOLD.Ability.Dex.long',
+  con: 'THRESHOLD.Ability.Con.long',
+  int: 'THRESHOLD.Ability.Int.long',
+  wis: 'THRESHOLD.Ability.Wis.long',
+  cha: 'THRESHOLD.Ability.Cha.long',
+};
+
+THRESHOLD.abilityAbbreviations = {
+  str: 'THRESHOLD.Ability.Str.abbr',
+  dex: 'THRESHOLD.Ability.Dex.abbr',
+  con: 'THRESHOLD.Ability.Con.abbr',
+  int: 'THRESHOLD.Ability.Int.abbr',
+  wis: 'THRESHOLD.Ability.Wis.abbr',
+  cha: 'THRESHOLD.Ability.Cha.abbr',
+};
